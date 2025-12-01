8a42d93e2b8f02337ddb6ea3507c9dba8e4fe781
Gorst <andinh.arobid@gmail.com>
2025-12-01 17:33:24 +0700

    create component Square can check constraints in 9x9 box

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,86 @@
-import { Square } from "@/public/components/square";
+const _ = undefined;
 
-const sudokuNumberList = [1, 2, 3 ,4 , 5, 6, 7, 8, 9]
-
-const _ = undefined
-
-const sudokuMiniBoardExample = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+export const sudokuMiniBoard = [
+  [1, 4, 5],
+  [6, 2, 7],
+  [_, _, _],
 ];
 
-const sudokuMiniBoard = [
-  [1, _, _],
-  [_, 2, _],
-  [_, _, 3],
-]
+//cell.rowIndex, cell.colIndex
 
-function solvingSudokuSquare({miniboard} : {miniboard:Array<number>})  {
-  let value
-  return (value);
-}
+const Square = ({ value }: { value?: number | undefined }) => {
+  const isEmpty = value === undefined || value === null;
+
+  const sudokuNumberList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  function possibleNumbers() {
+    const usedNumbers = sudokuMiniBoard.flat().filter((n) => n !== undefined);
+
+    return sudokuNumberList.filter((n) => !usedNumbers.includes(n));
+  }
+
+  if (!isEmpty) {
+    return (
+      <div className="border border-black w-[90px] h-[90px] flex justify-center items-center font-bold text-2xl">
+        {value}
+      </div>
+    );
+  }
+
+  if (possibleNumbers().length == 1) {
+    return (
+      <div className="border border-black w-[90px] h-[90px] flex justify-center items-center font-bold text-2xl">
+        {possibleNumbers()[0]}
+      </div>
+    );
+  }
+
+  return (
+    <div className="border border-black w-[90px] h-[90px] grid grid-cols-3 text-gray-600">
+      <span className="flex items-center justify-center w-[30px] h-[30px]">
+        {possibleNumbers().includes(1) && 1}
+      </span>
+      <span className="flex items-center justify-center w-[30px] h-[30px]">
+        {possibleNumbers().includes(2) && 2}
+      </span>
+      <span className="flex items-center justify-center w-[30px] h-[30px]">
+        {possibleNumbers().includes(3) && <span>3</span>}
+      </span>
+      <span className="flex items-center justify-center w-[30px] h-[30px]">
+        {possibleNumbers().includes(4) && 4}
+      </span>
+      <span className="flex items-center justify-center w-[30px] h-[30px]">
+        {possibleNumbers().includes(5) && 5}
+      </span>
+      <span className="flex items-center justify-center w-[30px] h-[30px]">
+        {possibleNumbers().includes(6) && 6}
+      </span>
+      <span className="flex items-center justify-center w-[30px] h-[30px]">
+        {possibleNumbers().includes(7) && 7}
+      </span>
+      <span className="flex items-center justify-center w-[30px] h-[30px]">
+        {possibleNumbers().includes(8) && 8}
+      </span>
+      <span className="flex items-center justify-center w-[30px] h-[30px]">
+        {possibleNumbers().includes(9) && 9}
+      </span>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <div className="bg-white h-screen text-black">
-      {/* <div className="mx-auto w-[450px] grid grid-cols-9"></div>
-       */}
-      <div className="mx-auto w-[150px]">
-        {sudokuMiniBoard.map((row, rowIndex) => (
-          <div key={rowIndex} className="grid grid-cols-3">
-            {row.map((cell, cellIndex) => (
-              <Square key={cellIndex} value={cell} />
-            ))}
-          </div>
-        ))}
-      </div>
-      <div className="flex justify-center mt-6">
-        <button className="bg-green-400 rounded-3xl py-2 px-5 ">Solve</button>
+    <div className="bg-white h-screen text-black flex justify-center items-center">
+      <div>
+        <div className="m-auto w-[270px]">
+          {sudokuMiniBoard.map((row, rowIndex) => (
+            <div key={rowIndex} className="grid grid-cols-3">
+              {row.map((cell, cellIndex) => (
+                <Square key={cellIndex} value={cell} />
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
